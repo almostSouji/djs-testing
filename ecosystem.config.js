a8200cdbfd25ba2fec6d06e88882281a74cc68c1
almostSouji <timoqueezle@gmail.com>
2018-11-11 09:46:20 +0100

    fix: cwd for ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,19 @@ module.exports = {
 		{
 			name: 'djs-stable',
 			script: './stable/index.js',
+			cwd: "./stable"
 		},
 		// dev
 		{
 			name: 'djs-dev',
 			script: './dev/index.js',
+			cwd: "./dev"
 		},
 		// master
 		{
 			name: 'djs-master',
 			script: './master/index.js',
+			cwd: "./master"
 		}
 	]
 };
