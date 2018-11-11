49358e8e2ba7d4b1ad2239c41d8f9879c5d719ce
almostSouji <timoqueezle@gmail.com>
2018-11-11 10:15:24 +0100

    feat: test command templates

---
+++ master/commands/test.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'test',
+	description: 'tsetcmd',
+	aliases: ['t'],
+	ownerOnly: true,
+	execute(msg, args) {
+		// BEGINNING OF TEST CODE
+
+		// END OF TESTCODE
+	}
+};
