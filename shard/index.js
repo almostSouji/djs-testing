528676cb965c8fc1f95dc8f175c49765418f22e0
almostSouji <timoqueezle@gmail.com>
2019-10-09 09:18:34 +0200

    chore: cleanup
    * remove redundant index2.js (dev)
    * remove whitespace (shard)

--- shard/index.js
+++ shard/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'your-token-goes-here' });
+const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN });
 
 manager.spawn();
 manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
