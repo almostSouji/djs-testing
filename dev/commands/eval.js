4e1a65fe8b1c9aff27a989ce0b5d4b2c894b0dc7
almostSouji <timoqueezle@gmail.com>
2019-10-09 09:17:32 +0200

    fix(dev): display correct version in eval

--- dev/commands/eval.js
+++ dev/commands/eval.js
@@ -26,7 +26,7 @@ module.exports = {
 			let response = '';
 
 			response += `\`\`\`js\n${clean(inspect(evaled, { depth: 0 }), msg.client.token)}\n\`\`\``;
-			response += `• d.js 11.4-dev`;
+			response += `• d.js 11.5-dev`;
 			response += ` • Type: \`${typeof evaled}\``;
 			response += ` • time taken: \`${(((hrStop[0] * 1e9) + hrStop[1])) / 1e6}ms\``;
 
