aeb09815ecbeec668b4a1dc008f3076178f052df
almostSouji <timoqueezle@gmail.com>
2019-10-09 09:15:25 +0200

    chore: streamline test command
    * add more redundant aliases (Discord, discord, msg, message)

--- shard/commands/test.js
+++ shard/commands/test.js
@@ -1,12 +1,14 @@
-/* eslint-disable no-unused-vars */
-const Discord = require('discord.js');
+const Discord = require('discord.js'); // eslint-disable-line no-unused-vars
+const discord = require('discord.js'); // eslint-disable-line no-unused-vars
 
 module.exports = {
 	name: 'test',
 	description: 'tsetcmd',
 	aliases: ['t'],
 	ownerOnly: true,
-	execute(msg, args) {
-
+	execute(msg, args) { // eslint-disable-line no-unused-vars
+		const message = msg; // eslint-disable-line no-unused-vars
+		// BEGINNING OF TEST CODE
+		// END OF TEST CODE
 	}
 };
--- stable/commands/test.js
+++ stable/commands/test.js
@@ -1,13 +1,14 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); // eslint-disable-line no-unused-vars
+const discord = require('discord.js'); // eslint-disable-line no-unused-vars
 
 module.exports = {
 	name: 'test',
-	description: 'tsetcmd',
+	description: 'testcmd',
 	aliases: ['t'],
 	ownerOnly: true,
-	execute(msg, args) {
+	execute(msg, args) { // eslint-disable-line no-unused-vars
+		const message = msg; // eslint-disable-line no-unused-vars
 		// BEGINNING OF TEST CODE
-
-		// END OF TESTCODE
+		// END OF TEST CODE
 	}
 };
