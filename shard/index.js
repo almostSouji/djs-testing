13b71375407dfec3a9620b9bb2c74e4fee739616
almostSouji <timoqueezle@gmail.com>
2020-03-03 18:04:43 +0100

    chore!: remove sharding boiler

--- shard/index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN });
-
-manager.spawn();
-manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
