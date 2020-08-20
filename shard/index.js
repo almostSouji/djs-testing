71e3ffc2539d889863cc28d193795d89d19173e9
almostSouji <timoqueezle@gmail.com>
2020-08-20 12:15:33 +0200

    feat: shard test suite

---
+++ shard/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN, shardList: [0, 1], totalShards: 2 });
+
+manager.spawn();
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
