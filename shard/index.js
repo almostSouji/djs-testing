2b1a72c1c2f37da79281b868df7607a92636cb6e
almostSouji <timoqueezle@gmail.com>
2021-07-02 14:46:14 +0200

    chore(*): remove outdated subprojects

--- shard/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN, shardList: [0, 1], totalShards: 2 });
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
